f0d97623845bd73114916a35ff1ed77dcc9f2c41
Viktor Kuroljov <viktor@ku.family>
2023-02-06 22:59:44 +0200

    feat: create tasks local only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,108 @@
+'use client';
+
+import { formatDistanceStrict } from "date-fns";
+import { useState, ChangeEventHandler, FormEventHandler, Key, useEffect } from "react"
+
+interface Task {
+  title: String,
+  timeline: number[],
+  createdAt: number,
+}
+
+const SECONDS = 1000
+const MINUTE = SECONDS * 60
+const HOUR = MINUTE * 60
+
+function elapsed(sinceMs: number): string {
+  const now = Date.now()
+  let diff = now - sinceMs
+
+  const hours = Math.floor(diff / HOUR)
+  diff -= hours * HOUR
+
+  const minutes = Math.floor(diff / MINUTE)
+  diff -= minutes * MINUTE
+
+  const seconds = Math.floor(diff / SECONDS)
+
+  const fmt = []
+
+  if (hours) fmt.push(`${hours}h`)
+  if (minutes) fmt.push(`${minutes}m`)
+  if (seconds) fmt.push(`${seconds}s`)
+
+  return fmt.join(' ')
+}
+
+function createTask({ title, createdAt }: Pick<Task, 'title' | 'createdAt'>): Task {
+  return {
+    title,
+    timeline: [createdAt],
+    createdAt,
+  }
+}
+
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [newTask, setNewTask] = useState({ desc: '' })
+
+  const handleTaskCreate: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    setTasks(tasks => ([...tasks, createTask({ title: newTask.desc, createdAt: Date.now() })]))
+    setNewTask({ desc: '' })
+  }
+
+  const handleTaskDescChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const val = e.target.value
+    setNewTask({ desc: val })
+  }
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTasks(tasks => {
+        return tasks.map(task => ({ ...task }))
+      })
+    }, 1000)
+
+    return () => {
+      clearInterval(id)
+    }
+  }, [])
+
   return (
-    <h1 className="text-2xl">Hello there love :3</h1>
+    <div className="m-16 flex flex-col">
+
+      <form className="flex bg-gray-100" onSubmit={handleTaskCreate}>
+        <div className="m-4 flex">
+          <button type="button" className="uppercase font-bold text-blue-600">start</button>
+        </div>
+        <input
+          type="text"
+          className="w-full p-4 bg-transparent"
+          placeholder="what are you working on?"
+          onChange={handleTaskDescChange}
+          value={newTask.desc}
+        />
+      </form>
+
+      <div className="rounded-lg w-[40rem] mt-8 p-4 m-auto grid gap-4">
+        <h3 className="uppercase font-thin text-xl">active.</h3>
+        {tasks.length === 0 ? (
+          <p className="font-thin text-center">you have no running tasks currently.</p>
+        ) : null}
+        {tasks.map(task => (
+          <div key={task.title as Key} className="flex p-2 items-center justify-between">
+            <p>{task.title}</p>
+            <p className="text-gray-600 font-bold text-sm border border-blue-100 bg-blue-100 p-1 rounded-lg">{elapsed(task.createdAt) || '0s'}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="rounded-lg w-[40rem] mt-8 p-4 m-auto grid gap-4">
+        <h3 className="uppercase font-thin text-xl">paused.</h3>
+        <p className="font-thin text-center">you have no paused tasks currently.</p>
+      </div>
+
+    </div>
   )
 }
