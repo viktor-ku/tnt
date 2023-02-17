d4b6e4f3761987bfd700da51dfced4bd4aa5e984
Viktor Kuroljov <viktor@ku.family>
2023-02-17 17:54:19 +0200

    layout the home page?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@
 export default function Home() {
   return (
     <div>
-      Home.
+
+      <div className="bg-indigo-500 w-full h-48 -mb-24"></div>
+
+      <div className="mx-auto w-2/3">
+        <p className="lowercase font-normal text-5xl text-white translate-y-[.35rem] select-none">Tasks</p>
+        <div className="bg-white shadow-lg p-4">
+        </div>
+      </div>
+
     </div>
   )
 }
